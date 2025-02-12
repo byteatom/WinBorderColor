e5cb6a91b316485221c9764a0d79d3f49cfe1456
byteatom <byteatom@qq.com>
2025-02-12 14:13:44 +0800

    添加项目文件。

---
+++ hook/hook.cpp
@@ -0,0 +1,32 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <dwmapi.h>
+
+#include "winutil.h"
+
+extern "C" LRESULT CALLBACK callWndProcRet(
+    _In_ int    nCode,
+    _In_ WPARAM wParam,
+    _In_ LPARAM lParam
+) {
+    
+    #pragma EXPORT_FUNC
+
+    auto cwpRets = (PCWPRETSTRUCT)lParam;
+    auto msg = cwpRets->message;
+    auto hwnd = cwpRets->hwnd;
+    auto style = GetWindowLong(hwnd, GWL_STYLE);
+
+    if (nCode < 0
+        || !((style & WS_OVERLAPPED) || (style & WS_OVERLAPPEDWINDOW))
+        || !(msg == WM_ACTIVATE || msg == WM_ACTIVATEAPP || msg == WM_SHOWWINDOW))
+        return CallNextHookEx(0, nCode, wParam, lParam);
+
+    const COLORREF borderColor = 0x00008800;
+    auto result = DwmSetWindowAttribute(hwnd, DWMWA_BORDER_COLOR, &borderColor, sizeof(borderColor));
+    if (result != S_OK)
+        winLog("DwmSetWindowAttribute:{:x}", result);
+
+    return CallNextHookEx(0, nCode, wParam, lParam);
+}
+
---
+++ loader/loader.cpp
@@ -0,0 +1,75 @@
+﻿#include <SDKDDKVer.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "winutil.h"
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    const wchar_t className[] = L"MainWnd";    
+
+    WNDCLASSEXW wcex = {};
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.lpfnWndProc = MainWndProc;
+    wcex.hInstance = hInstance;
+    wcex.lpszClassName = className;
+    RegisterClassExW(&wcex);
+
+    auto wnd = CreateWindowW(className, L"", 0,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, HWND_MESSAGE, nullptr, hInstance, nullptr);
+    if (!wnd) {
+        winLogLastError("CreateWindowW");
+        return FALSE;
+    }
+
+    USHORT processMachine;
+    USHORT nativeMachine;
+    if (!IsWow64Process2(GetCurrentProcess(), &processMachine, &nativeMachine)) {
+        winLogLastError("IsWow64Process2");
+        return FALSE;
+    }
+    auto dll = LoadLibraryW(processMachine == IMAGE_FILE_MACHINE_I386 ? L"hook32.dll" : L"hook64.dll");
+    if (!dll) {
+        winLogLastError("LoadLibraryW");
+        return FALSE;
+    }
+
+    auto callWndProcRet = (HOOKPROC)GetProcAddress(dll, "callWndProcRet");
+    if (!callWndProcRet) {
+        winLogLastError("GetProcAddress");
+        return FALSE;
+    }
+
+    auto callWndProcRetHook = SetWindowsHookExW(WH_CALLWNDPROCRET, callWndProcRet, dll, 0);
+    if (!callWndProcRetHook) {
+        winLogLastError("SetWindowsHookExW");
+        return FALSE;
+    }
+
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);        
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(callWndProcRetHook);
+
+    return (int)msg.wParam;
+}
